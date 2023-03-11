efb07374379980478ea3b4c77fe908e28db37d63
FelixFe26 <felix.fe015@gmail.com>
2023-03-11 16:52:00 -0300

    fisrt commit

---
+++ aula1/src/aula1/aula1.java
@@ -0,0 +1,27 @@
+package aula1;
+
+import aula1.Model.Gato;
+
+public class aula1 {
+
+	public static void main(String[] args) {
+	
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+			  
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+    
+}
+
+
+
+
+
+
+
+ class Livros {
+	 private String nome;
+	 private String npag;
+ }
