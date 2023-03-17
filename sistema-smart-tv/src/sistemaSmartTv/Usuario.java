f097132ae5c3ba47b7e1c754f3e3b876fb975a57
FelixFe26 <felix.fe015@gmail.com>
2023-03-17 18:16:04 -0300

    SmartTv

---
+++ sistema-smart-tv/src/sistemaSmartTv/Usuario.java
@@ -0,0 +1,26 @@
+package sistemaSmartTv;
+
+public class Usuario {
+
+	public static void main(String[] args) {
+
+		SmartTv smartTv = new SmartTv();
+ 
+		smartTv.diminuirVolume();
+		smartTv.diminuirVolume();
+		smartTv.diminuirVolume();
+		smartTv.aumentarVolume();
+		
+		smartTv.mudarCanal(15);
+		System.out.println("Canal Atual ? : " +smartTv.canal);
+		System.out.println("Volume Atual ? : " +smartTv.volume);
+		System.out.println("TV ligada ? " + smartTv.ligada);
+		System.out.println("Canal Atual ? : " +smartTv.canal);
+		System.out.println("Volume Atual ? : " +smartTv.volume);
+		
+		smartTv.ligar();
+		System.out.println("Novo Status -> TV ligada?" + smartTv.ligada);
+		
+	}
+
+}
