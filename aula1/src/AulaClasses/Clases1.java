f6d22fb5b6764c2d05952833bb501829be799556
FelixFe26 <felix.fe015@gmail.com>
2023-03-18 12:55:06 -0300

    OperadoresRelacionais

--- aula1/src/AulaClasses/Clases1.java
+++ aula1/src/AulaClasses/Clases1.java
@@ -3,17 +3,9 @@ package AulaClasses;
 public class Clases1 {
 
 	public static void main(String[] args) {
-
-		String primeiroNome = "Francisco";
-		String segundoNome= "Felix";
-
-		String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+					
+		System.out.println("Oi Turma, fui executado pelo Terminal");
 		
-		System.out.println(nomeCompleto);
-	}
 	
-	public static String nomeCompleto (String primeiroNome, String segundoNome) {
-		return primeiroNome.concat(" ").concat(segundoNome);
-	}
-	
+	}	
 }
